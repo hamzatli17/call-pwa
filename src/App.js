eacc8e5be8c9ab97c4c0eefc2799e957a096ee4a
hamza.tlili <hamza.tlili@steps.tn>
2022-12-15 16:23:18 +0100

    call s

--- src/App.js
+++ src/App.js
@@ -788,7 +788,7 @@ export class App extends Component {
               {this.state.phone}
             </span>
             <div>
-			+{this.state.countryCodeValue}{this.state.phone}
+			
               <a href="tel:+21622944673">
                 <FaPhone size="30" className="nav-linker" />
               </a>
